96af0dc09c914be2d45af187ebe06b47db5d0d52
Federico Blanco <banco.federicoandres@gmail.com>
2022-03-20 18:42:41 -0300

    2

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ console.log(body);
 
 const menu = document.getElementById("menu")
 console.log(menu)
-menu.addEventListener("click",()=>{
-    
+menu.addEventListener("click", () => {
+
     const divMenu = document.createElement("div")
     divMenu.className = "hola"
-    divMenu.textContent ="1111111111111"
+    divMenu.textContent = "1111111111111"
     divMenu.style.width = "100px"
-    divMenu.style.background="#ffff"
+    divMenu.style.background = "#ffff"
     body.appendChild(divMenu)
 })
