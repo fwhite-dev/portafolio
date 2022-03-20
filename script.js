55e453249b45b7039af320388e1dc58d3177609a
Federico Blanco <banco.federicoandres@gmail.com>
2022-03-20 12:41:47 -0300

    1

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+console.log(body);
+
+const menu = document.getElementById("menu")
+console.log(menu)
+menu.addEventListener("click",()=>{
+    
+    const divMenu = document.createElement("div")
+    divMenu.className = "hola"
+    divMenu.textContent ="1111111111111"
+    divMenu.style.width = "100px"
+    divMenu.style.background="#ffff"
+    body.appendChild(divMenu)
+})
